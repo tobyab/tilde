abfc41ca7352fd21e8664ad39d866159751ca523
Toby <mrtobez08@gmail.com>
2022-10-09 20:46:54 +0100

    editing/preview states!

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,14 +1,15 @@
 import Head from "next/head";
 import Nav from "../components/nav"
-import { useState, useCallback } from "react"
+import { useState, useReducer, useCallback } from "react"
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
 
 function Home() {
+  const [reader, setReader] = useState(false)
   const [doc, setDoc] = useState<string>(`
-  Here is a sentace with *italics*! And, here's another with **bold** font.
+  Here is a sentence with *italics*! And, here's another with **bold** font.
 
-  > Merry christmas, because here's one with a ~strikethrough~!
+  > Happy birthday, because here's one with a ~strikethrough~!
   * While we're at it, here's a list for you!
   * [x] never
   * [x] gonna
@@ -17,26 +18,34 @@ function Home() {
   * [x] up
   `)
 
-  const handleChange = useCallback(newDoc => {
-    setDoc(newDoc)
-  }, [])
   return (
-    <div className="bg-red-400">
+    <div>
       <Head>
         <title>Tilde</title>
       </Head>
       {/*<Nav/>*/}
+      <button
+          onClick={() => setReader(setReader => !setReader)}
+          className="float-right"
+        >
+        Click me!
+      </button>
         <div className="flex justify-center bg-red-400 h-screen mt-16 mx-8 space-x-2">
-          <textarea
-            className="w-1/2 h-screen"
-            onChange={(e) => setDoc(e.target.value)}
-            autoFocus
-          />
-          <ReactMarkdown 
-            children={doc} 
-            remarkPlugins={[[remarkGfm, { singleTilde: true }]]}
-            className="w-1/2"
-          />
+          {!reader &&(
+            <textarea
+              className="w-screen h-screen"
+              onChange={(e) => setDoc(e.target.value)}
+              value={doc || ""}
+              autoFocus
+            />
+          )}
+          {reader && (
+            <ReactMarkdown 
+              children={doc} 
+              remarkPlugins={[[remarkGfm, { singleTilde: true }]]}
+              className="w-screen"
+            />
+          )}
         </div>
     </div>
   );
